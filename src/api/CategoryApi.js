db2f5dd56a18ffceacd5d4cdbccdda8dcb5c8111
Ryan Harmon <heyharmon@gmail.com>
2022-01-31 21:46:10 -0700

    Update api endpoints

--- src/api/CategoryApi.js
+++ src/api/CategoryApi.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-    baseURL: 'http://cms-v1.test/api',
-    // baseURL: 'https://cms-v1-api.bloomcu.com/api',
+    // baseURL: 'http://cms-api-v1.test/api',
+    baseURL: 'https://cms-api-v1.bloomcu.com/api',
     withCredentials: false,
     headers: {
         Accept: 'application/json',
